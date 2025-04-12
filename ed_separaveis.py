3f3b2a804f72fade5d1893c83b3a5ae7279b776d
Axl Andrade <axlsandrade@outlook.com>
2025-04-12 19:12:04 -0300

    Finalizado as EDs. Separaveis e adicionado o arquivo das EDs de 1 ordem

--- ed_separaveis.py
+++ ed_separaveis.py
@@ -17,6 +17,21 @@ def euler(f, y0, x):
         y.append(y[-1] + h*f(x[i-1],y[-1]))
     return np.array(y)
 
+#Método de Runge-Kutta de 4ª ordem
+
+def rk4(f, y0, x):
+    y = [y0]
+    for i in range(1, len(x)):
+        h = x[i] - x[i-1]
+        xi = x[i-1]
+        yi = y[-1]
+        k1 = f(xi, yi)
+        k2 = f(xi + h/2, yi + h/2 * k1)
+        k3 = f(xi + h/2, yi + h/2 * k2)
+        k4 = f(xi + h, yi + h * k3)
+        y.append(yi + (h/6) * (k1 + 2*k2 + 2*k3 + k4)/6)
+    return np.array(y)
+
 #Malha de x com passo fixo
 
 h = 0.1
@@ -27,12 +42,18 @@ y0 = 1
 
 y_euler = euler(f, y0, x)
 
+y_rk4 = rk4(f, y0, x)
+
 y_exata = y_analitica(x)
 
 #Plot
 
 plt.plot(x, y_euler, label='Euler', linestyle ='--', color='blue')
 plt.plot(x, y_exata, label='Exata', linestyle='-', color='red')
+plt.plot(x, y_rk4, label='Runge-Kutta 4ª ordem', linestyle=':', color='green')
+plt.plot(x, abs(y_euler - y_exata), label='Erro Euler', linestyle='--', color='brown', alpha=0.5)
+plt.plot(x, abs(y_rk4 - y_exata), label='Erro RK4', linestyle=':', color='black', alpha=0.5)
+plt.xticks(np.round(np.arange(0, 1.1, 0.1), 1))
 plt.title('Comparação entre soluções numérica e exata')
 plt.xlabel('x')
 plt.ylabel('y')
